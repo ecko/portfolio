a29142686f5825bb86aa76ef4a920bea69fe3223
Michael <mike@ecko.ca>
2013-12-28 00:31:51 -0500

    included css files, js and some temp images.
    also have a basic layout for the portfolio page

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta charset="utf-8">
+
+	<title>Portfolio</title>
+	
+	<!--
+	<meta name="author" content="**">
+	<meta name="keywords" content="**">
+	<meta name="description" content="**">
+	-->
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	
+	<link rel="stylesheet" href="inc/css/toast.css">
+	<link rel="stylesheet" href="inc/css/normalize.css">
+	<link rel="stylesheet" href="inc/css/style.css">
+	<script src="inc/js/jquery-1.10.2.min.js"></script>
+
+
+</head>
+<body>
+
+	<header id="header">
+		<nav>
+			<div class="container">
+				<div class="grid">
+					<div class="unit span-grid">
+						<h2>&raquo; Michael Simion </h2>
+						<ul id="links">
+							<li>
+								<a href="#">About</a>
+							</li>
+							<li><a href="#">Work</a></li>
+							<li>
+								<a href="#">Services</a>
+							</li>
+							<li>
+								<a href="#">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
+	
+	<div id="main">
+		<div class="container">
+			<div id="work" class="grid">
+				<h1>Work</h1>
+				
+				<div class="unit one-of-three">
+					<a href="#">
+						<img src="img/test1.jpg">
+						
+					</a>
+				</div>
+				<div class="unit one-of-three">
+					<a href="#">
+						<img src="img/test2.jpg">
+						
+						<div class="details">
+							<h3>Test Name</h3>
+							<p>list of of the technologies used...</p>
+							<p>Maybe have the date here?</p>
+						</div>
+					</a>
+				</div>
+				<div class="unit one-of-three">
+					<a href="#">3</a>
+				</div>
+				
+				<div class="unit one-of-three">
+					<a href="#">
+						<img src="img/no_preview.gif">
+						
+					</a>
+				</div>
+				<div class="unit one-of-three">
+					<a href="#">5</a>
+				</div>
+				<div class="unit one-of-three">
+					<a href="#">6</a>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
+	
+	<footer>
+		<div class="container">
+			<div class="unit span-grid">
+				asdf
+			</div>
+		</div>
+	</footer>
+
+</body>
+</html>
